8e281e14cbfbede136c79031c81dc0d6ae66e8e4
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-07-28 19:23:37 +0200

    Refactoring the code

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -8,21 +8,15 @@ type Props = {
   query: string;
 };
 function Gallery(props: Props) {
-  if (props.items.length === 0) {
-    if (props.query.length === 0) {
-      return <NoSearch />;
-    }
-
-    return <NoResult query={props.query} />;
-  }
+  const { items, query } = props;
 
   return (
     <>
       <h4 className="text-center text-3xl my-10">
-        Search results for "{props.query}"
+        Search results for "{query}"
       </h4>
       <div className="my-7 columns-1 md:columns-2 lg:columns-4 gap-5">
-        {props.items.map((item, index) => (
+        {items.map((item, index) => (
           <Photo key={index} item={item} />
         ))}
       </div>
