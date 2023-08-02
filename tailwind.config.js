c7a8d69961a375f53eb609ce228e17892e342701
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-08-02 08:45:25 +0200

    Changing all the bad things on the site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        body: "#F3F3F3",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
