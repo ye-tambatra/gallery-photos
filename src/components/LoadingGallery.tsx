346be19795c25eaac0d6a932d2445ce608485435
ye-tambatra <nofiniaina.tambatra@gmail.com>
2023-08-09 19:27:46 -0500

    Add key to map

--- src/components/LoadingGallery.tsx
+++ src/components/LoadingGallery.tsx
@@ -1,21 +1,21 @@
 import Skeleton from "react-loading-skeleton";
 
 function LoadingGallery() {
-  function renderSkeleton() {
-    const skeleton = [];
-    for (let i = 0; i < 8; i++) {
-      skeleton.push(<Skeleton className="w-full h-[350px]" />);
+    function renderSkeleton() {
+        const skeleton = [];
+        for (let i = 0; i < 8; i++) {
+            skeleton.push(<Skeleton className="w-full h-[350px]" key={i} />);
+        }
+        return skeleton;
     }
-    return skeleton;
-  }
 
-  return (
-    <>
-      <div className="my-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-        {renderSkeleton()}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="my-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                {renderSkeleton()}
+            </div>
+        </>
+    );
 }
 
 export default LoadingGallery;
